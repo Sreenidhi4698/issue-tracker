b1df21dc3d835f90985ebc85f1c139590acd11b8
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-10-18 23:48:18 +0530

    Added additonal loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Box, Card, Flex } from '@radix-ui/themes';
 import React from 'react';
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box>
+      <Skeleton className='max-w-xl'></Skeleton>
+      <Flex className='space-x-3' my='2'>
+        <Skeleton width='5rem'></Skeleton>
+        <Skeleton width='8rem'></Skeleton>
+      </Flex>
+      <Card className='prose mt-4'>
+        <Skeleton count={3}></Skeleton>
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailPage;
