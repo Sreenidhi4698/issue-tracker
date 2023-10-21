c04e95f3c906ce5c528699cfcf4dd528cd4ae9c9
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-10-21 23:23:05 +0530

    Added filtering with status

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,16 +2,29 @@
 import React from 'react';
 import { Select } from '@radix-ui/themes';
 import { Status } from '@prisma/client';
+import { useRouter } from 'next/navigation';
 
 const IssueStatusFilter = () => {
   const statuses: { label: string; value?: Status }[] = [
     { label: 'All' },
     { label: 'Open', value: 'OPEN' },
-    { label: 'In Progress', value: 'CLOSED' },
-    { label: 'Closed', value: 'IN_PROGRESS' },
+    { label: 'In Progress', value: 'IN_PROGRESS' },
+    { label: 'Closed', value: 'CLOSED' },
   ];
+
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status
+          ? status == 'ALL'
+            ? ''
+            : `?status=${status}`
+          : '';
+        router.push('/issues/list' + query);
+      }}
+    >
       <Select.Trigger placeholder='Filter by status...'></Select.Trigger>
       <Select.Content>
         {statuses.map((status) => (
