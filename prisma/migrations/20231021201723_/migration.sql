3965a7a0f6cd3c73747f51d73b3c53dfe53efbac
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-10-22 01:47:48 +0530

    Changed relation mode to prisma

--- prisma/migrations/20231021201723_/migration.sql
+++ prisma/migrations/20231021201723_/migration.sql
@@ -1,29 +1,15 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Issue` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(255) NULL,
 
-  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `verificationrequest` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_user_id_fkey`;
-
--- DropForeignKey
-ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_user_id_fkey`;
-
--- DropTable
-DROP TABLE `accounts`;
-
--- DropTable
-DROP TABLE `sessions`;
-
--- DropTable
-DROP TABLE `users`;
-
--- DropTable
-DROP TABLE `verificationrequest`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Account` (
@@ -76,9 +62,3 @@ CREATE TABLE `VerificationToken` (
     UNIQUE INDEX `VerificationToken_token_key`(`token`),
     UNIQUE INDEX `VerificationToken_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
