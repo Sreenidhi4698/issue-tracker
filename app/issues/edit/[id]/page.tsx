a43b3b35884036a3571ec4a147d5cb99e8cecc6f
Sreenidhi4698 <b.sreenidhi@gmail.com>
2023-10-22 01:26:35 +0530

    Optimize performance using react cahce

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -2,6 +2,7 @@ import prisma from '@/prisma/client';
 import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
 import IssueFormSkeleton from './loading';
+import { cache } from 'react';
 
 const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
   ssr: false,
@@ -12,10 +13,16 @@ interface Props {
   params: { id: string };
 }
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: {
+      id: issueId,
+    },
+  })
+);
+
 const page = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
 
   if (!issue) notFound();
 
@@ -25,11 +32,7 @@ const page = async ({ params }: Props) => {
 export default page;
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   return {
     title: issue?.title,
     description: 'Edit details of issue - ' + issue?.id,
